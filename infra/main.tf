4f0707cc0515d8f1a79c6a6710be532c8d118d32
Yeongho Kim <yeonghoey@gmail.com>
2018-12-09 19:05:37 +0900

    Add infra

---
+++ infra/main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = "nijk-225007"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-c"
+}
+
+resource "google_sql_database_instance" "nijk" {
+  name = "nijk-master"
+  database_version = "MYSQL_5_7"
+  region = "asia-northeast1"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "nijk" {
+  instance  = "${google_sql_database_instance.nijk.name}"
+  name      = "nijk"
+  charset   = "utf8"
+}
+
+resource "google_sql_user" "nijk" {
+  instance = "${google_sql_database_instance.nijk.name}"
+  name     = "nijk"
+}
