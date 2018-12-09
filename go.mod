91fd277b5ddfe135d6d177854b714bbc16abb618
Yeongho Kim <yeonghoey@gmail.com>
2018-12-09 21:00:28 +0900

    Implement scorer

--- go.mod
+++ go.mod
@@ -1,9 +1,12 @@
 module github.com/yeonghoey/nijk
 
 require (
+	cloud.google.com/go v0.34.0 // indirect
+	github.com/GoogleCloudPlatform/cloudsql-proxy v0.0.0-20181204152105-d3e0c892beca
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
 	github.com/gin-gonic/gin v1.3.0
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
 	github.com/golang/protobuf v1.2.0 // indirect
 	github.com/json-iterator/go v1.1.5 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
@@ -13,8 +16,10 @@ require (
 	github.com/stretchr/testify v1.2.2 // indirect
 	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
 	golang.org/x/net v0.0.0-20181201002055-351d144fa1fc // indirect
+	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890 // indirect
 	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
 	golang.org/x/sys v0.0.0-20181206074257-70b957f3b65e // indirect
+	google.golang.org/api v0.0.0-20181206211257-1a5ef82f9af4 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
--- scorer/cmd/main.go
+++ scorer/cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/mysql"
 	"github.com/yeonghoey/nijk/scorer"
 )
 
@@ -13,15 +16,72 @@ const (
 	b = 0.75
 )
 
+// TODO: Deduplicate queries
+const paradigSchema = `
+CREATE TABLE IF NOT EXISTS paradigmatic
+(
+  preset VARCHAR(64),
+  this   VARCHAR(64),
+  that   VARCHAR(64),
+  score  DOUBLE,
+  PRIMARY KEY (preset, this, that)
+);`
+
+const paradigInsert = `
+INSERT INTO paradigmatic (preset, this, that, score) VALUES (?, ?, ?, ?);`
+
+const syntagSchema = `
+CREATE TABLE IF NOT EXISTS syntagmatic
+(
+  preset VARCHAR(64),
+  this   VARCHAR(64),
+  that   VARCHAR(64),
+  score  DOUBLE,
+  PRIMARY KEY (preset, this, that)
+);`
+
+const syntagInsert = `
+INSERT INTO syntagmatic (preset, this, that, score) VALUES (?, ?, ?, ?);`
+
+type dbWrapper struct {
+	db   *sql.DB
+	last string
+	err  error
+}
+
+func (dbw *dbWrapper) exec(query string, args ...interface{}) {
+	if dbw.err == nil {
+		_, err := dbw.db.Exec(query, args...)
+		dbw.last = query
+		dbw.err = err
+	}
+}
+
 func main() {
+	db, err := mysql.Dial("nijk-225007:asia-northeast1:nijk-master", "nijk")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	dbw := dbWrapper{db, "", nil}
+	dbw.exec("USE nijk;")
+	dbw.exec(paradigSchema)
+	dbw.exec(syntagSchema)
+	if dbw.err != nil {
+		log.Fatalf("%s: %v\n", dbw.last, dbw.err)
+	}
+
 	reader := bufio.NewReader(os.Stdin)
 	collection := scorer.NewCollection(reader, k, b)
 
 	collection.Paradigmatic(func(a, b string, score float64) {
-		fmt.Printf("%s %s %.2f\n", a, b, score)
+		fmt.Printf("Paradigmatic: %s %s %.2f\n", a, b, score)
+		db.Exec(paradigInsert, "python", a, b, score)
 	})
 
 	collection.Syntagmatic(func(a, b string, score float64) {
-		fmt.Printf("%s %s %.2f\n", a, b, score)
+		fmt.Printf("Syntagmatic: %s %s %.2f\n", a, b, score)
+		db.Exec(syntagInsert, "python", a, b, score)
 	})
 }
