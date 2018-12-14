5008ad453a27a945a63b646503f67d0d256a820d
Yeongho Kim <yeonghoey@gmail.com>
2018-12-14 11:04:22 +0900

    Refactor scorer/cmd

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nijk
+Nijk is for helping programmers come up with good names.
+
+
+## Caveat
+
+This is my(yeongho2@illinois.edu) course project for
+Text Information Systems of [MCS-DS](https://cs.illinois.edu/academics/graduate/professional-mcs-program/online-master-computer-science-data-science).
+
--- scorer/cmd/main.go
+++ scorer/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"sync"
 	"sync/atomic"
 
@@ -14,13 +15,22 @@ import (
 // TODO: Parameterize these constants
 const (
 	numWorkers = 64
+	topN       = 100
 
 	k = 1.2
 	b = 0.75
-
-	paradigmaticThreshold = 0.5
-	syntagmaticThreshold  = 0.5
 )
+
+type entry struct {
+	that  string
+	score float64
+}
+
+type scoreDesc []entry
+
+func (a scoreDesc) Len() int           { return len(a) }
+func (a scoreDesc) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a scoreDesc) Less(i, j int) bool { return a[i].score > a[j].score }
 
 var preset string = os.Args[1]
 
@@ -29,21 +39,26 @@ func main() {
 	collection := scorer.NewCollection(reader, k, b)
 
 	log.Printf("Run Paradigmatic")
-	collection.Paradigmatic(numWorkers, newHandler("paradigmatic", paradigmaticThreshold))
+
+	parEntries := map[string][]entry{}
+	collection.Paradigmatic(numWorkers, newHandler(parEntries))
+	outputEntries("paradigmatic", parEntries)
 
 	log.Printf("Run Syntagmatic")
-	collection.Syntagmatic(numWorkers, newHandler("syntagmatic", syntagmaticThreshold))
+	synEntries := map[string][]entry{}
+	collection.Syntagmatic(numWorkers, newHandler(synEntries))
+	outputEntries("syntagmatic", synEntries)
 }
 
-func newHandler(relation string, threshold float64) func(a, b string, score float64) {
+func newHandler(entries map[string][]entry) func(a, b string, score float64) {
 	var mutex = &sync.Mutex{}
 	var processed int32
 	return func(a, b string, score float64) {
-		if score < threshold {
+		if score < 0.01 {
 			return
 		}
 		mutex.Lock()
-		fmt.Println(insertQuery(preset, relation, a, b, score))
+		entries[a] = append(entries[a], entry{b, score})
 		mutex.Unlock()
 
 		incremented := atomic.AddInt32(&processed, 1)
@@ -54,8 +69,24 @@ func newHandler(relation string, threshold float64) func(a, b string, score floa
 
 }
 
-func insertQuery(preset, relation string, this, that string, score float64) string {
+func outputEntries(relation string, entries map[string][]entry) {
+	for this, es := range entries {
+		sort.Sort(scoreDesc(es))
+		for _, e := range es[:min(len(es), topN)] {
+			fmt.Println(insertQuery(relation, this, e.that, e.score))
+		}
+	}
+}
+
+func insertQuery(relation string, this, that string, score float64) string {
 	table := fmt.Sprintf("`%s_%s`", preset, relation)
 	return fmt.Sprintf("INSERT INTO %s VALUES (\"%s\", \"%s\", %.5f);",
 		table, this, that, score)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
